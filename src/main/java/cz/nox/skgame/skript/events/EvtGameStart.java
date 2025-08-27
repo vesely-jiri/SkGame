73eab1ce8c06f2389b41a47d116fe402a830b5cf
Jiří Veselý <vesely.jiri1998@gmail.com>
2025-08-27 15:24:48 +0200

    feat: implemented events GameStart and GameStop

--- src/main/java/cz/nox/skgame/skript/events/EvtGameStart.java
+++ src/main/java/cz/nox/skgame/skript/events/EvtGameStart.java
@@ -1,27 +1,49 @@
 package cz.nox.skgame.skript.events;
 
+import ch.njol.skript.Skript;
 import ch.njol.skript.lang.Literal;
 import ch.njol.skript.lang.SkriptEvent;
 import ch.njol.skript.lang.SkriptParser;
+import ch.njol.skript.registrations.EventValues;
+import cz.nox.skgame.api.game.event.GameStartEvent;
+import cz.nox.skgame.api.game.model.GameMode;
+import cz.nox.skgame.api.game.model.Session;
 import org.bukkit.event.Event;
 import org.jetbrains.annotations.Nullable;
 
 /**
  * Called when gamemode of session starts
  */
-public class EvtGameStart extends SkriptEvent {
+@SuppressWarnings("unused")
+public class    EvtGameStart extends SkriptEvent {
+    private Session session;
+    private Literal<GameMode> gameMode;
+
+    static {
+        Skript.registerEvent("GameStart", EvtGameStart.class, GameStartEvent.class,
+                "[%string%] game start",
+                "game [%string%] start"
+        );
+        EventValues.registerEventValue(GameStartEvent.class, Session.class, GameStartEvent::getSession, EventValues.TIME_NOW);
+        EventValues.registerEventValue(GameStartEvent.class, GameMode.class, GameStartEvent::getGameMode, EventValues.TIME_NOW);
+    }
+
+    @SuppressWarnings("unchecked")
     @Override
-    public boolean init(Literal<?>[] literals, int i, SkriptParser.ParseResult parseResult) {
-        return false;
+    public boolean init(Literal<?>[] args, int i, SkriptParser.ParseResult parseResult) {
+        if (args[0] != null) {
+            this.gameMode = (Literal<GameMode>) args[0];
+        }
+        return true;
     }
 
     @Override
-    public boolean check(Event event) {
-        return false;
+    public boolean check(Event e) {
+        return this.gameMode.getSingle(e) == ((GameStartEvent) e).getGameMode();
     }
 
     @Override
     public String toString(@Nullable Event event, boolean b) {
-        return null;
+        return "on game " + gameMode.getSingle(event) + " start of session " + session;
     }
 }
--- src/main/java/cz/nox/skgame/skript/events/EvtGameStop.java
+++ src/main/java/cz/nox/skgame/skript/events/EvtGameStop.java
@@ -1,23 +1,45 @@
 package cz.nox.skgame.skript.events;
 
+import ch.njol.skript.Skript;
 import ch.njol.skript.lang.Literal;
 import ch.njol.skript.lang.SkriptEvent;
 import ch.njol.skript.lang.SkriptParser;
+import ch.njol.skript.registrations.EventValues;
+import cz.nox.skgame.api.game.event.GameStopEvent;
+import cz.nox.skgame.api.game.model.GameMode;
+import cz.nox.skgame.api.game.model.Session;
 import org.bukkit.event.Event;
 import org.jetbrains.annotations.Nullable;
 
 /**
  * Called when gamemode of session stops
  */
+@SuppressWarnings("unused")
 public class EvtGameStop extends SkriptEvent {
+    private Session session;
+    private Literal<GameMode> gamemode;
+
+    static {
+        Skript.registerEvent("GameStop", EvtGameStop.class, GameStopEvent.class,
+                "[%string%] game stop",
+                "game [%string%] stop"
+        );
+        EventValues.registerEventValue(GameStopEvent.class, Session.class, GameStopEvent::getSession, EventValues.TIME_NOW);
+        EventValues.registerEventValue(GameStopEvent.class, GameMode.class, GameStopEvent::getGameMode, EventValues.TIME_NOW);
+    }
+
+    @SuppressWarnings("unchecked")
     @Override
-    public boolean init(Literal<?>[] literals, int i, SkriptParser.ParseResult parseResult) {
-        return false;
+    public boolean init(Literal<?>[] args, int i, SkriptParser.ParseResult parseResult) {
+        if (args[0] != null) {
+            this.gamemode = (Literal<GameMode>) args[0];
+        }
+        return true;
     }
 
     @Override
-    public boolean check(Event event) {
-        return false;
+    public boolean check(Event e) {
+        return this.gamemode.getSingle(e) == ((GameStopEvent) e).getGameMode();
     }
 
     @Override
