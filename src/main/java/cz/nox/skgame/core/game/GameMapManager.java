16f2b78720505d8bae93e661f9347ba3dc8d7771
Jiří Veselý <vesely.jiri1998@gmail.com>
2025-08-27 15:17:17 +0200

    feat: add serialization, getById and registration

--- src/main/java/cz/nox/skgame/core/game/GameMapManager.java
+++ src/main/java/cz/nox/skgame/core/game/GameMapManager.java
@@ -1,7 +1,12 @@
 package cz.nox.skgame.core.game;
 
+import cz.nox.skgame.SkGame;
 import cz.nox.skgame.api.game.model.GameMap;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.YamlConfiguration;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.*;
 
 public class GameMapManager {
@@ -16,20 +21,53 @@ public class GameMapManager {
         return gameMapManager;
     }
 
-    public void createGameMap(String id) {
+    public void loadFromFile(File file) {
+        if (!file.exists()) return;
+        YamlConfiguration config = YamlConfiguration.loadConfiguration(file);
+        ConfigurationSection baseSection = config.getConfigurationSection("gamemaps");
+        if (baseSection == null) return;
+        for (String key : baseSection.getKeys(false)) {
+            ConfigurationSection section = config.getConfigurationSection("gamemaps." + key);
+            if (section == null) continue;
+            Map<String, Object> mapData = section.getValues(true);
+            GameMap map = GameMap.deserialize(mapData);
+            maps.put(map.getId(),map);
+        }
+    }
+    public void saveToFile(File file) {
+        YamlConfiguration config = YamlConfiguration.loadConfiguration(file);
+        config.set("maps",null);
+        for (GameMap map : maps.values()) {
+            config.createSection("maps." + map.getId(), map.serialize());
+        }
+        try {
+            config.save(file);
+        } catch (IOException e) {
+            SkGame.getInstance().getLogger().severe("Error while saving map config(" + file.getName() + ":"
+                    + e.getMessage());
+        }
+    }
+
+    public GameMap getGameMapById(String id) {
+        return maps.get(id);
+    }
+
+    public void registerGameMap(String id) {
         if (maps.containsKey(id)) return;
         GameMap map = new GameMap(id);
         maps.put(id,map);
         setLastCreatedGameMap(map);
     }
-    public void deleteGameMap(String id) {
+    public void unregisterGameMap(String id) {
         maps.remove(id);
+    }
+    public boolean isMapRegistered(String id) {
+        return maps.containsKey(id);
     }
 
     public boolean isMapClaimed(String gameMapId) {
         return claimedMaps.contains(gameMapId);
     }
-
     public void addMapToClaimed(GameMap gameMap) {
         claimedMaps.add(gameMap.getId());
     }
--- src/main/java/cz/nox/skgame/core/game/GameModeManager.java
+++ src/main/java/cz/nox/skgame/core/game/GameModeManager.java
@@ -1,13 +1,20 @@
 package cz.nox.skgame.core.game;
 
+import cz.nox.skgame.SkGame;
 import cz.nox.skgame.api.game.model.GameMode;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.MemorySection;
+import org.bukkit.configuration.file.YamlConfiguration;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 public class GameModeManager {
 
     private static GameModeManager gameModeManager;
-    private Map<String, GameMode> gameModes;
+    private Map<String, GameMode> gameModes = new HashMap<>();
     private GameMode lastCreatedGameMode;
 
     public static GameModeManager getInstance() {
@@ -15,10 +22,53 @@ public class GameModeManager {
         return gameModeManager;
     }
 
+    public GameMode getGameModeById(String id) {
+        return gameModes.get(id);
+    }
+
+    public void loadFromFile(File file) {
+        if (!file.exists()) return;
+        YamlConfiguration config = YamlConfiguration.loadConfiguration(file);
+        ConfigurationSection baseSection = config.getConfigurationSection("gamemodes");
+        if (baseSection == null) return;
+        for (String key : baseSection.getKeys(false)) {
+            ConfigurationSection section = config.getConfigurationSection("gamemodes." + key);
+            if (section == null) continue;
+            Map<String, Object> gmData = section.getValues(true);
+            GameMode gm = GameMode.deserialize(gmData);
+            gameModes.put(gm.getId(),gm);
+        }
+    }
+    public void saveToFile(File file) {
+        YamlConfiguration config = YamlConfiguration.loadConfiguration(file);
+        config.set("gamemodes",null);
+        for (GameMode gm : gameModes.values()) {
+            config.createSection("gamemodes." + gm.getId(), gm.serialize());
+        }
+        try {
+            config.save(file);
+        } catch (IOException e) {
+            SkGame.getInstance().getLogger().severe("Error while saving GameModes: " + e.getMessage());
+        }
+    }
+
     public void registerGameMode(String id) {
         if (gameModes.containsKey(id)) return;
         GameMode gameMode = new GameMode(id);
         gameModes.put(id,gameMode);
         lastCreatedGameMode = gameMode;
+    }
+    public void unregisterGameMode(String id) {
+        gameModes.remove(id);
+    }
+    public boolean isRegistered(String id) {
+        return gameModes.containsKey(id);
+    }
+
+    public GameMode getLastCreatedGameMode() {
+        return lastCreatedGameMode;
+    }
+    public void setLastCreatedGameMode(GameMode gameMode) {
+        this.lastCreatedGameMode = gameMode;
     }
 }
