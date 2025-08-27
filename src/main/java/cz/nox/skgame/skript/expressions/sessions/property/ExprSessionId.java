696d5f056c4d65f66bb708803ebad5e63ba873bb
Jiří Veselý <vesely.jiri1998@gmail.com>
2025-08-27 15:28:06 +0200

    style: remove unused imports

--- src/main/java/cz/nox/skgame/skript/expressions/sessions/property/ExprSessionId.java
+++ src/main/java/cz/nox/skgame/skript/expressions/sessions/property/ExprSessionId.java
@@ -1,9 +1,7 @@
 package cz.nox.skgame.skript.expressions.sessions.property;
 
 import ch.njol.skript.expressions.base.SimplePropertyExpression;
-import ch.njol.util.coll.CollectionUtils;
 import cz.nox.skgame.api.game.model.Session;
-import org.bukkit.event.Event;
 import org.jetbrains.annotations.Nullable;
 
 @SuppressWarnings("unused")
@@ -19,13 +17,6 @@ public class ExprSessionId extends SimplePropertyExpression<Session, String> {
     public @Nullable String convert(Session session) {
         return session.getId();
     }
-
-//    @Override
-//    protected String[] get(Event event, Session[] source) {
-//        Session session = getExpr().getSingle(event);
-//        if (session == null) return null;
-//        return CollectionUtils.array(session.getId());
-//    }
 
     @Override
     protected String getPropertyName() {
