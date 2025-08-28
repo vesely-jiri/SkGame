87b4f0de119ccb191ce99da6b4f888d1015370c8
Jiří Veselý <vesely.jiri1998@gmail.com>
2025-08-28 11:36:28 +0200

    feat: add new effect to disband session

---
+++ src/main/java/cz/nox/skgame/skript/effects/EffSessionDelete.java
@@ -0,0 +1,42 @@
+package cz.nox.skgame.skript.effects;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.lang.Effect;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.util.Kleenean;
+import cz.nox.skgame.api.game.model.Session;
+import cz.nox.skgame.core.game.SessionManager;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.Nullable;
+
+@SuppressWarnings("unused")
+public class EffSessionDelete extends Effect {
+    private static final SessionManager sessionManager = SessionManager.getInstance();
+    private Expression<Session> session;
+
+    static {
+        Skript.registerEffect(EffSessionDelete.class,
+                "(delete|disband) [session] %session%"
+        );
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public boolean init(Expression<?>[] exprs, int i, Kleenean kleenean, SkriptParser.ParseResult parseResult) {
+        this.session = (Expression<Session>) exprs[0];
+        return true;
+    }
+
+    @Override
+    protected void execute(Event event) {
+        Session session = this.session.getSingle(event);
+        if (session == null) return;
+        sessionManager.deleteSession(session.getId());
+    }
+
+    @Override
+    public String toString(@Nullable Event event, boolean b) {
+        return "delete session with id " + this.session.toString(event,b);
+    }
+}
