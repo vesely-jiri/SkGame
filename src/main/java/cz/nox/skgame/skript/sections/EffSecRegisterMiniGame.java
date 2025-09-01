e5564ac7af2a4a6186acd875096031ac029e66f5
Jiří Veselý <vesely.jiri1998@gmail.com>
2025-09-01 12:37:10 +0200

    fix: registerMiniGame section default expression(now pointing on event-minigame)

--- src/main/java/cz/nox/skgame/skript/sections/EffSecRegisterMiniGame.java
+++ src/main/java/cz/nox/skgame/skript/sections/EffSecRegisterMiniGame.java
@@ -3,7 +3,10 @@ package cz.nox.skgame.skript.sections;
 import ch.njol.skript.Skript;
 import ch.njol.skript.config.SectionNode;
 import ch.njol.skript.lang.*;
+import ch.njol.skript.registrations.EventValues;
 import ch.njol.util.Kleenean;
+import cz.nox.skgame.api.game.event.MiniGameRegisterEvent;
+import cz.nox.skgame.api.game.model.MiniGame;
 import cz.nox.skgame.core.game.MiniGameManager;
 import org.bukkit.event.Event;
 import org.jetbrains.annotations.Nullable;
@@ -13,18 +16,26 @@ import java.util.List;
 @SuppressWarnings("unused")
 public class EffSecRegisterMiniGame extends EffectSection {
 
-    private static final MiniGameManager MINI_GAME_MANAGER = MiniGameManager.getInstance();
+    private static final MiniGameManager miniGameManager = MiniGameManager.getInstance();
     private Expression<String> id;
+    private Trigger trigger;
 
     static {
         Skript.registerSection(EffSecRegisterMiniGame.class,
                 "(register|create) [new] minigame (with|from) id %string%"
         );
+        EventValues.registerEventValue(MiniGameRegisterEvent.class, MiniGame.class,
+              MiniGameRegisterEvent::getMiniGame);
     }
 
     @SuppressWarnings("unchecked")
     @Override
-    public boolean init(Expression<?>[] exprs, int i, Kleenean kleenean, SkriptParser.ParseResult parseResult, SectionNode sectionNode, List<TriggerItem> list) {
+    public boolean init(Expression<?>[] exprs, int i, Kleenean kleenean, SkriptParser.ParseResult parseResult,
+                        SectionNode sectionNode, List<TriggerItem> list) {
+        if (hasSection()) {
+            trigger = loadCode(sectionNode,"minigame register", MiniGameRegisterEvent.class);
+        }
+
         this.id = (Expression<String>) exprs[0];
         return true;
     }
@@ -33,9 +44,14 @@ public class EffSecRegisterMiniGame extends EffectSection {
     protected @Nullable TriggerItem walk(Event event) {
         String id = this.id.getSingle(event);
 
-        // TODO - Check if script file contains required events (and effects?)
+        if (miniGameManager.getMiniGameById(id) != null) return super.walk(event,false);
+        miniGameManager.registerMiniGame(id);
 
-        MINI_GAME_MANAGER.registerMiniGame(id);
+        MiniGame mg = miniGameManager.getMiniGameById(id);
+
+        if (trigger != null) {
+            TriggerItem.walk(trigger, new MiniGameRegisterEvent(mg));
+        }
         return super.walk(event,false);
     }
 
