b68f209785053cefd90d294eb5daa0f974358ae2
Jiří Veselý <vesely.jiri1998@gmail.com>
2025-10-10 15:05:12 +0200

    feat: add Session disband Bukkit event

---
+++ src/main/java/cz/nox/skgame/api/game/event/SessionDisbandEvent.java
@@ -0,0 +1,27 @@
+package cz.nox.skgame.api.game.event;
+
+import cz.nox.skgame.api.game.model.Session;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+public class SessionDisbandEvent extends Event {
+    private static final HandlerList handlers = new HandlerList();
+    private final Session session;
+
+    public SessionDisbandEvent(Session session) {
+        this.session = session;
+    }
+
+    public Session getSession() {
+        return this.session;
+    }
+
+    @Override
+    public @NotNull HandlerList getHandlers() {
+        return handlers;
+    }
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+}
