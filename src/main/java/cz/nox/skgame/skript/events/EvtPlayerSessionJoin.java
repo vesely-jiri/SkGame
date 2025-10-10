2eb0ec28b3861123c66c9b2652231d7a2be495e8
Jiří Veselý <vesely.jiri1998@gmail.com>
2025-10-10 14:48:59 +0200

    feat: implementation of player session join event and leave event

--- src/main/java/cz/nox/skgame/skript/events/EvtPlayerSessionJoin.java
+++ src/main/java/cz/nox/skgame/skript/events/EvtPlayerSessionJoin.java
@@ -1,25 +1,39 @@
 package cz.nox.skgame.skript.events;
 
+import ch.njol.skript.Skript;
 import ch.njol.skript.lang.Literal;
 import ch.njol.skript.lang.SkriptEvent;
 import ch.njol.skript.lang.SkriptParser;
+import ch.njol.skript.registrations.EventValues;
+import cz.nox.skgame.api.game.event.GamePlayerSessionJoin;
+import cz.nox.skgame.api.game.model.Session;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.jetbrains.annotations.Nullable;
 
 @SuppressWarnings("unused")
 public class EvtPlayerSessionJoin extends SkriptEvent {
-    @Override
-    public boolean init(Literal<?>[] literals, int i, SkriptParser.ParseResult parseResult) {
-        return false;
+
+    static {
+        Skript.registerEvent("PlayerSessionJoin", EvtPlayerSessionJoin.class, GamePlayerSessionJoin.class,
+                "player session join"
+        );
+        EventValues.registerEventValue(GamePlayerSessionJoin.class, Player.class, GamePlayerSessionJoin::getPlayer, EventValues.TIME_NOW);
+        EventValues.registerEventValue(GamePlayerSessionJoin.class, Session.class, GamePlayerSessionJoin::getSession, EventValues.TIME_NOW);
     }
 
     @Override
-    public boolean check(Event event) {
-        return false;
+    public boolean init(Literal<?>[] literals, int i, SkriptParser.ParseResult parseResult) {
+        return true;
+    }
+
+    @Override
+    public boolean check(Event e) {
+        return true;
     }
 
     @Override
     public String toString(@Nullable Event event, boolean b) {
-        return null;
+        return "on player session join";
     }
 }
--- src/main/java/cz/nox/skgame/skript/events/EvtPlayerSessionLeave.java
+++ src/main/java/cz/nox/skgame/skript/events/EvtPlayerSessionLeave.java
@@ -1,25 +1,39 @@
 package cz.nox.skgame.skript.events;
 
+import ch.njol.skript.Skript;
 import ch.njol.skript.lang.Literal;
 import ch.njol.skript.lang.SkriptEvent;
 import ch.njol.skript.lang.SkriptParser;
+import ch.njol.skript.registrations.EventValues;
+import cz.nox.skgame.api.game.event.GamePlayerSessionLeave;
+import cz.nox.skgame.api.game.model.Session;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.jetbrains.annotations.Nullable;
 
 @SuppressWarnings("unused")
 public class EvtPlayerSessionLeave extends SkriptEvent {
+
+    static {
+        Skript.registerEvent("PlayerSessionLeave", EvtPlayerSessionLeave.class, GamePlayerSessionLeave.class,
+                "player session leave"
+        );
+        EventValues.registerEventValue(GamePlayerSessionLeave.class, Player.class,GamePlayerSessionLeave::getPlayer, EventValues.TIME_NOW);
+        EventValues.registerEventValue(GamePlayerSessionLeave.class, Session.class, GamePlayerSessionLeave::getSession, EventValues.TIME_NOW);
+    }
+
     @Override
     public boolean init(Literal<?>[] literals, int i, SkriptParser.ParseResult parseResult) {
-        return false;
+        return true;
     }
 
     @Override
     public boolean check(Event event) {
-        return false;
+        return true;
     }
 
     @Override
     public String toString(@Nullable Event event, boolean b) {
-        return null;
+        return "on player session leave";
     }
 }
