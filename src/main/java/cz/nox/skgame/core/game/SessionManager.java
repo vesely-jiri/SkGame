0f29fda49142fddd5f381f2ad78e60113bcca559
Jiří Veselý <vesely.jiri1998@gmail.com>
2025-10-10 14:59:24 +0200

    feat: player session join and leave events implemented to Sessionmanager

--- src/main/java/cz/nox/skgame/core/game/SessionManager.java
+++ src/main/java/cz/nox/skgame/core/game/SessionManager.java
@@ -1,15 +1,19 @@
 package cz.nox.skgame.core.game;
 
+import cz.nox.skgame.api.game.event.GamePlayerSessionJoin;
+import cz.nox.skgame.api.game.event.GamePlayerSessionLeave;
 import cz.nox.skgame.api.game.event.SessionCreateEvent;
 import cz.nox.skgame.api.game.model.Session;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.*;
 
-public class SessionManager {
+public class SessionManager implements Listener {
 
     private static SessionManager manager;
     private final Map<String,Session> sessions = new HashMap<>();
@@ -69,5 +73,15 @@ public class SessionManager {
     @Nullable
     public Session getLastCreatedSession() {
         return this.lastCreatedSession;
+    }
+
+    @EventHandler
+    public void onPlayerSessionJoin(GamePlayerSessionJoin e) {
+        playerToSession.put(e.getPlayer().getUniqueId(),e.getSession().getId());
+    }
+
+    @EventHandler
+    public void onPlayerSessionLeave(GamePlayerSessionLeave e) {
+        playerToSession.remove(e.getPlayer().getUniqueId());
     }
 }
