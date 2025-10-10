9c684171807a7eed249814971883567e1663af23
Jiří Veselý <vesely.jiri1998@gmail.com>
2025-10-10 15:05:50 +0200

    feat: add expression enabling dev to obtain session of a player

---
+++ src/main/java/cz/nox/skgame/skript/expressions/gameplayers/ExprGamePlayerSession.java
@@ -0,0 +1,55 @@
+package cz.nox.skgame.skript.expressions.gameplayers;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.ExpressionType;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.util.Kleenean;
+import ch.njol.util.coll.CollectionUtils;
+import cz.nox.skgame.api.game.model.Session;
+import cz.nox.skgame.core.game.SessionManager;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.Nullable;
+
+@SuppressWarnings("unused")
+public class ExprGamePlayerSession extends SimpleExpression<Session> {
+    private static final SessionManager sessionManager = SessionManager.getInstance();
+    private Expression<Player> player;
+
+    static {
+        Skript.registerExpression(ExprGamePlayerSession.class, Session.class, ExpressionType.SIMPLE,
+                "session of %player%"
+        );
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public boolean init(Expression<?>[] exprs, int i, Kleenean kleenean, SkriptParser.ParseResult parseResult) {
+        this.player = (Expression<Player>) exprs[0];
+        return true;
+    }
+
+    @Override
+    protected Session @Nullable [] get(Event event) {
+        Player player = this.player.getSingle(event);
+        if (player == null) return null;
+        return CollectionUtils.array(sessionManager.getSession(player));
+    }
+
+    @Override
+    public boolean isSingle() {
+        return true;
+    }
+
+    @Override
+    public Class<? extends Session> getReturnType() {
+        return Session.class;
+    }
+
+    @Override
+    public String toString(@Nullable Event event, boolean b) {
+        return "";
+    }
+}
