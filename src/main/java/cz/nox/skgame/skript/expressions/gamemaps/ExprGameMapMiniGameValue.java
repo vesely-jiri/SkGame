144727c46961c61f447ec9f237a3f464f2a0afa7
Jiří Veselý <vesely.jiri1998@gmail.com>
2025-10-30 13:02:32 +0100

    feat: add index/key to loops

--- src/main/java/cz/nox/skgame/skript/expressions/gamemaps/ExprGameMapMiniGameValue.java
+++ src/main/java/cz/nox/skgame/skript/expressions/gamemaps/ExprGameMapMiniGameValue.java
@@ -11,6 +11,7 @@ import ch.njol.util.coll.CollectionUtils;
 import cz.nox.skgame.api.game.model.GameMap;
 import cz.nox.skgame.api.game.model.MiniGame;
 import org.bukkit.event.Event;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 @SuppressWarnings("unused")
@@ -59,13 +60,18 @@ public class ExprGameMapMiniGameValue extends SimpleExpression<Object> {
         GameMap map = this.gameMap.getSingle(event);
         MiniGame mg = this.miniGame.getSingle(event);
         if (map == null || mg == null) return null;
+        String miniGameId = mg.getId();
         Object o;
         if (this.pattern == 0) {
             String k = this.key.getSingle(event);
             if (k == null) return null;
-            o = map.getMiniGameValue(mg.getId(), this.key.getSingle(event));
+            o = map.getMiniGameValue(miniGameId, this.key.getSingle(event));
         } else {
-            o = map.getAllMiniGameValues();
+            if (this.mark == 0) {
+                o = map.getMiniGameKeys(miniGameId);
+            } else {
+                o = map.getMiniGameValues(miniGameId);
+            }
         }
         if (o instanceof Object[]) {
             return (Object[]) o;
@@ -109,14 +115,50 @@ public class ExprGameMapMiniGameValue extends SimpleExpression<Object> {
     }
 
     @Override
-    public boolean isSingle() {
-        return this.pattern == 0;
+    public boolean isLoopOf(String input) {
+        if (input.equals("key") || input.equals("index")) {
+            return true;
+        }
+        return super.isLoopOf(input);
     }
 
     @Override
-    public String toString(@Nullable Event event, boolean b) {
-        return "map value " + this.key.toString(event,b)
-                + " of map " + this.gameMap.toString(event,b)
-                + " of minigame " + this.miniGame.toString(event,b);
+    public boolean isIndexLoop(String input) {
+        if (input.equals("key") || input.equals("index")) {
+            return true;
+        }
+        return KeyProviderExpression.super.isIndexLoop(input);
+    }
+
+    @Override
+    public @NotNull String @NotNull [] getArrayKeys(Event e) throws IllegalStateException {
+        GameMap gm = this.gameMap.getSingle(e);
+        MiniGame mg = this.miniGame.getSingle(e);
+        if (mg == null || gm == null) return new String[0];
+        return gm.getMiniGameKeys(mg.getId());
+    }
+
+    @Override
+    public boolean canReturnKeys() {
+        return (this.pattern == 1) && ((this.mark == 1));
+    }
+
+    @Override
+    public boolean isSingle() {
+        return (this.pattern == 0);
+    }
+
+    @Override
+    public String toString(@Nullable Event e, boolean b) {
+        if (this.pattern == 0) {
+            return "map value " + this.key.toString(e, b) +
+                    " of map " + this.gameMap.toString(e, b) +
+                    " of minigame " + this.miniGame.toString(e, b);
+        } else {
+            return "map " +
+                    ((this.mark == 1) ? "values" : "keys") +
+                    " of map " + this.gameMap.toString(e,b) +
+                    " of minigame " + this.miniGame.toString(e,b);
+        }
     }
 }
