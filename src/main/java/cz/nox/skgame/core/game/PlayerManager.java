b382c47c6d64af3431936ed70e53a85549f95a05
Jiří Veselý <vesely.jiri1998@gmail.com>
2025-11-12 12:27:58 +0100

    refactor: simplify creation of GamePlayer

--- src/main/java/cz/nox/skgame/core/game/PlayerManager.java
+++ src/main/java/cz/nox/skgame/core/game/PlayerManager.java
@@ -1,6 +1,7 @@
 package cz.nox.skgame.core.game;
 
 import cz.nox.skgame.api.game.model.GamePlayer;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import java.util.HashMap;
@@ -18,17 +19,11 @@ public class PlayerManager {
         return playerManager;
     }
 
-    public GamePlayer createPlayer(Player player) {
-        GamePlayer gamePlayer = new GamePlayer(player);
-        players.put(player.getUniqueId(),gamePlayer);
-        return gamePlayer;
-    }
-
     public void deletePlayer(Player player) {
         players.remove(player.getUniqueId());
     }
 
     public GamePlayer getPlayer(Player player) {
-        return players.computeIfAbsent(player.getUniqueId(), id -> new GamePlayer(player));
+        return players.computeIfAbsent(player.getUniqueId(), uuid -> new GamePlayer(Bukkit.getPlayer(uuid)));
     }
 }
