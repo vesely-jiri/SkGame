4b7f83d738149c7e718e4cf7e8dd07163d9c9218
Jiří Veselý <vesely.jiri1998@gmail.com>
2026-01-29 23:35:43 +0100

    test: CI

--- src/main/java/cz/nox/skgame/SkGame.java
+++ src/main/java/cz/nox/skgame/SkGame.java
@@ -32,6 +32,8 @@ public class SkGame extends JavaPlugin {
     public void onEnable() {
         long s = System.currentTimeMillis();
 
+        // CI test
+
         instance = this;
         this.logUtil = new LogUtil(instance);
 
